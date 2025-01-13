e8756afa6be4a20709c63536b25fc1aff3b788d0
Vanessa Bautista <66338789+jvaneb@users.noreply.github.com>
2025-01-13 18:00:48 -0500

    version 1.2.6 - Segundo ajuste slider

--- club50mas/assets/js/logic.js
+++ club50mas/assets/js/logic.js
@@ -103,32 +103,18 @@ function returnPage() {
     window.history.back();
 }
 
-// Ocultar card slider
-// document.addEventListener("DOMContentLoaded", function () {
-//     const currentPageCard = "credito-libranza";
-//     const replacementCardHTML = `
-//     <div class="card text-center">
-//         <div class="card-body d-flex flex-column">
-//         <div class="d-flex align-items-center mb-3">
-//             <img src="assets/tarjeta_posible.svg" alt="Nueva Opción" class="card-img-custom me-3">
-//             <h4 class="card-title mx-4">Tarjeta <br><span class="mth-txt-color-h4">Posible</span></h4>
-//         </div>
-//         <p class="card-text px-2">
-//              Tarjeta Posible: Flexibilidad, cuotas fijas, compras nacionales e internacionales, sin seguros deudores ni avances.
-//         </p>
-//         <a href="#" class="btn btn-success mth-btn-carousel mt-3 mx-auto">Conoce más</a>
-//         </div>
-//     </div>
-//     `;
+let items = document.querySelectorAll('.carousel .carousel-item')
 
-//     const carousel = document.getElementById("carouselExampleIndicators");
-//     const cards = carousel.querySelectorAll(".card-item");
-
-//     cards.forEach((card) => {
-//     const cardType = card.getAttribute("data-card");
-//         if (cardType === currentPageCard) {
-            
-//             card.innerHTML = replacementCardHTML;
-//         }
-//     });
-// });
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
