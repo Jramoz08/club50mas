ca38bd6916a413e718f3b749f5cf034f07693277
Jramoz08 <yesusrimos@gmail.com>
2025-01-14 11:45:53 -0500

    Version 1.2.5

--- club50mas/assets/js/logic.js
+++ club50mas/assets/js/logic.js
@@ -103,32 +103,24 @@ function returnPage() {
     window.history.back();
 }
 
-// Ocultar card slider
-// document.addEventListener("DOMContentLoaded", function () {
-//     const currentPageCard = "credito-libranza";
-//     const replacementCardHTML = `
-//     <div class="card text-center">
-//         <div class="card-body d-flex flex-column">
-//         <div class="d-flex align-items-center mb-3">
-//             <img src="assets/tarjeta_posible.svg" alt="Nueva Opción" class="card-img-custom me-3">
-//             <h4 class="card-title mx-4">Tarjeta <br><span class="mth-txt-color-h4">Posible</span></h4>
-//         </div>
-//         <p class="card-text px-2">
-//              Tarjeta Posible: Flexibilidad, cuotas fijas, compras nacionales e internacionales, sin seguros deudores ni avances.
-//         </p>
-//         <a href="#" class="btn btn-success mth-btn-carousel mt-3 mx-auto">Conoce más</a>
-//         </div>
-//     </div>
-//     `;
 
-//     const carousel = document.getElementById("carouselExampleIndicators");
-//     const cards = carousel.querySelectorAll(".card-item");
+const video = document.getElementById("myVideo");
+const playPauseBtn = document.getElementById("playPauseBtn");
 
-//     cards.forEach((card) => {
-//     const cardType = card.getAttribute("data-card");
-//         if (cardType === currentPageCard) {
-            
-//             card.innerHTML = replacementCardHTML;
-//         }
-//     });
-// });
+function togglePlayPause() {
+  if (video.paused) {
+    video.play();
+    playPauseBtn.innerHTML = '<img src="assets/video/pause.png" alt="pausa">'; // Imagen de pausa
+  } else {
+    video.pause();
+    playPauseBtn.innerHTML = '<img src="assets/video/play.png" alt="play">'; // Imagen de play
+  }
+}
+
+video.addEventListener('play', function () {
+  playPauseBtn.innerHTML = '<img src="assets/video/pause.png" alt="pausa">'; // Imagen de pausa
+});
+
+video.addEventListener('pause', function () {
+  playPauseBtn.innerHTML = '<img src="assets/video/play.png" alt="play">'; // Imagen de play
+});
